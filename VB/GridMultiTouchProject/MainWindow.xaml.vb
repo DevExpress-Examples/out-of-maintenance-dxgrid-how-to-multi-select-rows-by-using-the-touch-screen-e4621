4bddb0e23e9ede188ae1ad2dfafb3826cf28eefe
DevExpressExampleBot <fakeEmail>
2022-12-16 17:05:32 +0400

    autogenerate VB

--- VB/GridMultiTouchProject/MainWindow.xaml.vb
+++ VB/GridMultiTouchProject/MainWindow.xaml.vb
@@ -1,42 +1,37 @@
-﻿Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Shapes
-
 
 Namespace GridMultiTouchProject
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			ListPerson = New List(Of Person)()
-			CreatePerson()
-			DataContext = Me
-		End Sub
-	  Public Property ListPerson() As List(Of Person)
-		Private Sub CreatePerson()
-			For i As Integer = 0 To 49
-				ListPerson.Add(New Person(i))
-			Next i
-		End Sub
-	End Class
-	Public Class Person
-		Public Sub New(ByVal i As Integer)
-			FirstName = String.Format("FirstName - {0}", i)
-			LastName = String.Format("LastName - {0}", i)
-		End Sub
-		Public Property FirstName() As String
-		Public Property LastName() As String
+    Public Partial Class MainWindow
+        Inherits Window
 
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            ListPerson = New List(Of Person)()
+            CreatePerson()
+            DataContext = Me
+        End Sub
 
+        Public Property ListPerson As List(Of Person)
+
+        Private Sub CreatePerson()
+            For i As Integer = 0 To 50 - 1
+                ListPerson.Add(New Person(i))
+            Next
+        End Sub
+    End Class
+
+    Public Class Person
+
+        Public Sub New(ByVal i As Integer)
+            FirstName = String.Format("FirstName - {0}", i)
+            LastName = String.Format("LastName - {0}", i)
+        End Sub
+
+        Public Property FirstName As String
+
+        Public Property LastName As String
+    End Class
 End Namespace
